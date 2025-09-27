137fc84bfb309e2d02c2c40bbc6ed14556e696b9
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-09-27 22:50:36 +0300

    Added ks_ref

--- ks_std/src/ks_ref.rs
+++ ks_std/src/ks_ref.rs
@@ -1,7 +1,19 @@
+use core::global::utils::ks_error::KsError;
 use core::global::utils::ks_result::KsResult;
 use core::vm::environment::Environment;
+use core::vm::value::Value;
 use core::vm::variable::Variable;
 
 pub fn ks_ref(environment: &mut Environment, args: Vec<Variable>) -> KsResult<Variable> {
-    todo!()
+    if args.len() > 1 {
+        return Err(KsError::native("Too many arguments!"));
+    }
+
+    let variable = &args[0];
+    let reference = variable.reference()?;
+
+    Ok(Variable::empty(
+        Value::Integer(reference as i32), 
+        environment.depth()
+    ))
 }
