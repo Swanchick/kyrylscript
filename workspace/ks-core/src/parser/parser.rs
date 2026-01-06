bf54d6d2be68c2298c94328f714031c1ccdb3f4f
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-01-06 23:52:17 +0200

    Fixed bug with the saving parameter to the parent scope, not in the child one that we create

--- workspace/ks-core/src/parser/parser.rs
+++ workspace/ks-core/src/parser/parser.rs
@@ -134,9 +134,6 @@ impl Parser {
         let name = self.consume_identifier()?;
         let data_type = self.parse_data_type()?;
 
-        self.semantic_analyzer
-            .save_variable(name.clone(), data_type.clone());
-
         let parameter = Parameter {
             name: name,
             data_type: data_type,
@@ -335,6 +332,11 @@ impl Parser {
         }
 
         self.semantic_analyzer.enter_function_environment();
+
+        for parameter in &parameters {
+            self.semantic_analyzer
+                .save_variable(parameter.name.clone(), parameter.data_type.clone());
+        }
 
         let body = self.parse_block_statement()?;
 
@@ -887,6 +889,10 @@ impl Parser {
                 Some(Token::LeftParenthesis) => {
                     self.semantic_analyzer.enter_function_environment();
                     let parameters = self.parse_parameters()?;
+                    for parameter in &parameters {
+                        self.semantic_analyzer
+                            .save_variable(parameter.name.clone(), parameter.data_type.clone());
+                    }
 
                     let return_type = if self.match_token(&Token::Colon) {
                         self.parse_data_type()?
@@ -935,6 +941,10 @@ impl Parser {
         self.consume_token(Token::LeftParenthesis)?;
         self.semantic_analyzer.enter_function_environment();
         let parameters = self.parse_parameters()?;
+        for parameter in &parameters {
+            self.semantic_analyzer
+                .save_variable(parameter.name.clone(), parameter.data_type.clone());
+        }
 
         let return_type = if self.match_token(&Token::Colon) {
             let data_type = self.parse_data_type()?;
