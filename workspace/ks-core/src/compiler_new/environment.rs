c08f8aa60f307bf89488d76a58b9e5ac544bc467
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-03-07 23:02:08 +0200

    Deleted get variable_id in environment

--- workspace/ks-core/src/compiler_new/environment.rs
+++ workspace/ks-core/src/compiler_new/environment.rs
@@ -100,19 +100,6 @@ impl Environment {
         self.functions.insert(name.to_string(), function);
     }
 
-    pub fn variable_id(&self, name: &str) -> KsResult<VariableId> {
-        for scope in &self.variables {
-            if let Some(slot) = scope.get(name) {
-                return Ok(*slot.variable_id());
-            }
-        }
-
-        Err(KsError::parse(&format!(
-            "Cannot find variable by this name: {}",
-            name
-        )))
-    }
-
     pub fn slot(&self, name: &str) -> KsResult<&Slot> {
         for scope in &self.variables {
             if let Some(slot) = scope.get(name) {
