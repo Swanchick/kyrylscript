30434b23f1218be30806dfe863d2fea783776a84
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-03-24 22:57:00 +0200

    Updated the runner_loop

--- workspace/ks-vm-new/src/lib.rs
+++ workspace/ks-vm-new/src/lib.rs
@@ -8,4 +8,5 @@ mod vm;
 pub use ir::constant::Constant;
 pub use ir::instructions::Instruction;
 pub use ir::program::Program;
+pub use runner::Runner;
 pub use vm::VM;
--- workspace/ks-vm-new/src/runner.rs
+++ workspace/ks-vm-new/src/runner.rs
@@ -1,17 +1,11 @@
 use ks_global::utils::ks_result::KsResult;
 
-use super::Instruction;
 use super::types::Pointer;
 use super::vgs::VGS;
+use super::{Constant, Instruction};
 
 pub struct Runner {
     program_counter: Pointer,
-}
-
-impl From<Pointer> for Runner {
-    fn from(program_counter: Pointer) -> Self {
-        Self { program_counter }
-    }
 }
 
 impl Runner {
@@ -19,7 +13,16 @@ impl Runner {
         Self { program_counter: 0 }
     }
 
+    fn load_const(&mut self, constant: Constant) -> KsResult<()> {
+        todo!()
+    }
+
     pub fn step(&mut self, instruction: Instruction, vgs: &mut VGS) -> KsResult<()> {
+        match instruction {
+            Instruction::LoadConst(constant) => self.load_const(constant)?,
+            _ => todo!(),
+        }
+
         Ok(())
     }
 
