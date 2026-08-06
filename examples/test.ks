d494d3a09b68233a3efb940e6a5d9bca61977f37
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-08-06 19:49:24 +0300

    Shipped not working code

--- examples/test.ks
+++ examples/test.ks
@@ -1,13 +1,5 @@
-let numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+let a = 10;
 
-for i in range(len(numbers)) {
-    for j in range(len(numbers) - i - 1) {
-        let n1 = numbers[j]!;
-        let n2 = numbers[j + 1]!;
+let b = 20;
 
-        if n1 > n2 {
-            numbers[j] = n2;
-            numbers[j + 1] = n1;
-        }
-    }
-}
+let c = a + b;
--- workspace/ks-programator/src/main.rs
+++ workspace/ks-programator/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use ks_core::{compiler_new::compiler::CompilerNew, lexer::lexer::Lexer, parser::parser::Parser};
+use ks_global::utils::{ks_error::KsError, ks_result::KsResult};
+
+fn main() -> KsResult<()> {
+    let mut lexer = Lexer::load("examples/test.ks")?;
+    lexer.lexer()?;
+
+    let mut parser = Parser::new();
+    parser.set_tokens(lexer.get_tokens().to_vec(), lexer.get_token_pos().to_vec());
+    let block = parser.start()?;
+
+    let mut compiler = CompilerNew::new();
+    compiler.compile(block)?;
+
+    let program = compiler.program();
+    let mut bytes = program.serialize();
+    let bytes_len = bytes.len() as u32;
+    let mut bytes_len = bytes_len.to_le_bytes().to_vec();
+
+    let mut final_program = Vec::<u8>::with_capacity(bytes.len() + bytes_len.len());
+    final_program.append(&mut bytes_len);
+    final_program.append(&mut bytes);
+
+    println!("{:X?}", final_program);
+
+    let ports = serialport::available_ports().expect("Hello World");
+    for p in ports {
+        println!("{}", p.port_name);
+    }
+
+    let mut port = serialport::new("/dev/ttyACM0", 115_200)
+        .timeout(Duration::from_millis(1000))
+        .open()
+        .map_err(|e| KsError::runtime(&e.to_string()))?;
+
+    port.write_all(&final_program)
+        .map_err(|e| KsError::runtime(&e.to_string()))?;
+
+    port.flush().map_err(|e| KsError::runtime(&e.to_string()))?;
+
+    println!("Sent {} bytes", final_program.len());
+
+    Ok(())
 }
