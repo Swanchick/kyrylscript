1fbb17774c20fbe1e4bba91abc79175487b1c7e8
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-08-26 20:58:29 +0300

    Added proper size serialization

--- workspace/ks-core/src/compiler_new/instructions.rs
+++ workspace/ks-core/src/compiler_new/instructions.rs
@@ -5,8 +5,6 @@ use ks_vm_new::ir::instructions::{
     LDN, LDS, LDV, LDV8, LDV16, LE, LEN, LT, MUL, NCALL, NE, NOT, OR, RET, STR, SUB,
 };
 
-// use crate::types::{Arguments, CaptureSize, NativeId, Offset, VariableId};
-
 use crate::compiler_new::types::VariableId;
 
 use super::constant::Constant;
@@ -87,7 +85,7 @@ impl Instruction {
 
     fn load_const(&self, constant: &Constant) -> Vec<u8> {
         match constant {
-            Constant::Integer(integer) => self.opcode_value_u64_dynamic(LDI, *integer as u64),
+            Constant::Integer(integer) => self.compressed_i64(LDI8, LDI16, LDI32, LDI, *integer),
             Constant::Float(float) => self.opcode_value_u64(LDF, float.to_bits()),
             Constant::Boolean(boolean) => {
                 if *boolean {
@@ -122,6 +120,79 @@ impl Instruction {
         opcode
     }
 
+    fn compressed_u32(
+        &self,
+        instruction_8: u8,
+        instruction_16: u8,
+        instruction_32: u8,
+        number: u32,
+    ) -> Vec<u8> {
+        let mut opcode: Vec<u8> = vec![];
+
+        if let Ok(v) = u8::try_from(number) {
+            opcode.push(instruction_8);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else if let Ok(v) = u16::try_from(number) {
+            opcode.push(instruction_16);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else {
+            opcode.push(instruction_32);
+            opcode.append(&mut number.to_le_bytes().to_vec());
+        }
+
+        opcode
+    }
+
+    fn compressed_i32(
+        &self,
+        instruction_8: u8,
+        instruction_16: u8,
+        instruction_32: u8,
+        number: i32,
+    ) -> Vec<u8> {
+        let mut opcode: Vec<u8> = vec![];
+
+        if let Ok(v) = i8::try_from(number) {
+            opcode.push(instruction_8);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else if let Ok(v) = i16::try_from(number) {
+            opcode.push(instruction_16);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else {
+            opcode.push(instruction_32);
+            opcode.append(&mut number.to_le_bytes().to_vec());
+        }
+
+        opcode
+    }
+
+    fn compressed_i64(
+        &self,
+        instruction_8: u8,
+        instruction_16: u8,
+        instruction_32: u8,
+        instruction_64: u8,
+        number: i64,
+    ) -> Vec<u8> {
+        let mut opcode: Vec<u8> = vec![];
+
+        if let Ok(v) = i8::try_from(number) {
+            opcode.push(instruction_8);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else if let Ok(v) = i16::try_from(number) {
+            opcode.push(instruction_16);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else if let Ok(v) = i32::try_from(number) {
+            opcode.push(instruction_32);
+            opcode.append(&mut v.to_le_bytes().to_vec());
+        } else {
+            opcode.push(instruction_64);
+            opcode.append(&mut number.to_le_bytes().to_vec());
+        }
+
+        opcode
+    }
+
     pub fn to_bytes(self) -> Vec<u8> {
         match &self {
             Self::Add => vec![ADD],
@@ -142,23 +213,23 @@ impl Instruction {
             Self::Clone => vec![CPY],
             Self::ClearAcc => vec![CLR],
             Self::Return => vec![RET],
-            Self::Free(size) => self.opcode_value_u32(FREE, *size as u32),
-            Self::JumpIfFalse(offset) => self.opcode_value_u32(JZ, *offset as u32),
-            Self::JumpIfTrue(offset) => self.opcode_value_u32(JNZ, *offset as u32),
-            Self::Jump(offset) => self.opcode_value_u32(JMP, *offset as u32),
+            Self::Free(size) => self.compressed_u32(FREE8, FREE16, FREE, *size as u32),
+            Self::JumpIfFalse(offset) => self.compressed_i32(JZ8, JZ16, JZ, *offset),
+            Self::JumpIfTrue(offset) => self.compressed_i32(JNZ8, JNZ16, JNZ, *offset),
+            Self::Jump(offset) => self.compressed_i32(JMP8, JMP16, JMP, *offset),
             Self::Store => vec![STR],
             Self::Assign => vec![ASN],
-            Self::AssignVariable(variable_id) => self.opcode_value_u32(ASV, *variable_id),
+            Self::AssignVariable(variable_id) => {
+                self.compressed_u32(ASV8, ASV16, ASV, *variable_id)
+            }
             Self::AssignCollection => vec![ASC],
             Self::LoadConst(constant) => self.load_const(constant),
-            Self::LoadVar(variable_id) => self.opcode_value_u32(LDV, *variable_id),
-            Self::Call(arguments) => self.opcode_value_u32(CALL, *arguments as u32),
-            Self::CallNative(native_id, arguments) => {
-                self.native(*native_id as u32, *arguments as u32)
-            }
-            Self::LoadCapture(captured) => self.opcode_value_u32(LDCP, *captured),
-            Self::LoadFunction(size) => self.opcode_value_u32(LDFN, *size as u32),
-            Self::LoadCollection(size) => self.opcode_value_u32(LDC, *size as u32),
+            Self::LoadVar(variable_id) => self.compressed_u32(LDV8, LDV16, LDV, *variable_id),
+            Self::Call(arguments) => self.compressed_u32(CALL8, CALL16, CALL, *arguments),
+            Self::CallNative(native_id, arguments) => self.native(*native_id, *arguments),
+            Self::LoadCapture(captured) => self.compressed_u32(LDCP8, LDCP16, LDCP, *captured),
+            Self::LoadFunction(size) => self.compressed_u32(LDFN8, LDFN16, LDFN, *size as u32),
+            Self::LoadCollection(size) => self.compressed_u32(LDC8, LDC16, LDC, *size as u32),
             Self::LoadFromCollection => vec![LDFC],
             Self::CollectionLen => vec![LEN],
         }
