8f2c1ba8ae45fb962d5045dce8163ebb116b54d6
Kyryl Lebedenko <Kiryll.Swan@gmail.com>
2026-09-01 19:34:19 +0300

    Removed println

--- workspace/ks-vm-new/src/runner.rs
+++ workspace/ks-vm-new/src/runner.rs
@@ -812,8 +812,6 @@ impl Runner {
         let gvs = helper.gvs;
         let reader = ByteReader::new(self.pc, helper.instructions);
 
-        println!("{:X?}", helper.instruction);
-
         match helper.instruction {
             LDN => self.load_null(gvs),
             LBT => self.load_true(gvs),
